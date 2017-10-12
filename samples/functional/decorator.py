86319c1607ea7d444c5bc24b1bcc113d3cffe35e
liyanjiu <liyanjiu@outlook.com>
2017-10-12 16:43:43 +0800

    add stdlib

---
+++ samples/functional/decorator.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+增强now()函数的功能，但又不希望修改now()函数的定义，这种在代码运行期间动态增加功能的方式，称之为“装饰器”（Decorator）。
+本质上，decorator就是一个返回函数的高阶函数。所以，我们要定义一个能打印日志的decorator
+"""
+
+import functools
+
+
+def log(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kw):
+        print('call %s():' % func.__name__)
+        return func(*args, **kw)
+    return wrapper
+
+
+@log
+def now():
+    print('2015-3-25')
+
+now()
+
+
+def logger(text):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            print('%s %s():' % (text, func.__name__))
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+
+
+@logger('DEBUG')
+def today():
+    print('2015-3-25')
+
+today()
+print(today.__name__)
---
+++ stdlib/builtin_functions/builtin_func.md
@@ -0,0 +1,7 @@
+[Built-in Functions](https://docs.python.org/3/library/functions.html#format)
+--------------------
+ 
+![all](./sample/stdlib/builtin_functions/func.png)
+
+
+
